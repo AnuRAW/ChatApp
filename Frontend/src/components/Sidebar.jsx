344ab13eeea7790613fcc1444dd4a754d62166ae
AnuRaw <anuragrawat230@gmail.com>
2024-08-29 02:22:30 +0530

    .

--- Frontend/src/components/Sidebar.jsx
+++ Frontend/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import Conversations from "./Conversations.jsx"
-import LogoutBtn from "./Logoutbtn.jsx"
+import LogoutBtn from "./LogoutBtn.jsx"
 import SearchInput from "./SearchInput.jsx"
 
 const Sidebar = () => {
