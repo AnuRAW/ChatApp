bdefccc86454be57bbb0e5f9536527b204be4554
AnuRaw <anuragrawat230@gmail.com>
2024-08-28 14:53:48 +0530

    Socket part completed

--- Backend/server.js
+++ Backend/server.js
@@ -5,10 +5,16 @@ import authRoute from "./routes/auth.route.js";
 import messageRoute from "./routes/message.route.js";
 import userRoute from "./routes/user.route.js";
 import connectmongoDB from "./db/connectmongo.js";
-const app = express();
+import cors from "cors"
+import { app, server } from "./socket/Socket.js";
+
+
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
+app.use(cors({
+    origin: 'http://localhost:3000' // Allow this specific origin
+  }));
 app.use(express.json());// TO parse the incoming requests with json payload (form req.body).
 app.use(cookieParser());
 app.use("/api/auth",authRoute);
@@ -16,11 +22,12 @@ app.use("/api/messages",messageRoute);
 app.use("/api/users",userRoute);
 
 
+
 // app.get("/",(req,res)=>{
 // //rest rout http://localhost:5000/
 // res.send("Ready to work")
 // })
-app.listen(5000,() =>{
+server.listen(PORT,() =>{
     connectmongoDB();
     console.log(`Server Runing on port ${PORT}`)
 })
