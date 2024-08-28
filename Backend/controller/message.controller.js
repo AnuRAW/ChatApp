bf9004bbf319e0d01a39129345a83df0c354aced
AnuRaw <anuragrawat230@gmail.com>
2024-08-28 14:55:02 +0530

    4th part socket part completed

--- Backend/controller/message.controller.js
+++ Backend/controller/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/convo.models.js";
 import Message from "../models/mess.models.js";
+import { getReceiverSocketId,io } from "../socket/Socket.js";
 
 export const sendMessage = async(req,res)=>{
    try {
@@ -19,12 +20,22 @@ export const sendMessage = async(req,res)=>{
       senderId,
       receiverId,
       message,
-    })
+    });
+
     if(newMessage){
       conversation.messages.push(newMessage._id);
     }
+//await conversation.save()
+//await newMessage.save()
 
-    await Promise.all([conversation.save(),newMessage.save()])
+//this will be run in parallel
+    await Promise.all([conversation.save(), newMessage.save()])
+//SOCKET IO func will GO here
+      const receiverSocketId = getReceiverSocketId(receiverId);
+      if(receiverSocketId){
+        //io.to(<socket_id>).emit()used to send events to specific client
+        io.to(receiverSocketId).emit("newMessage",newMessage)
+      }
 
      res.status(201).json(newMessage);
    } catch (error) {
--- Frontend/src/components/Mess/Message.jsx
+++ Frontend/src/components/Mess/Message.jsx
@@ -1,17 +1,27 @@
-const Message = () => {
-  return (
-    <div className="chat chat-end">
-      <div className="chat-image avatar">
-        <div className="w-10 rounded-full">
-        <img src={
-            "https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-        } alt="Tailwind CSS chat bubble component" />
-        </div>
-      </div>
-      <div className={"chat-bubble text-white bg-blue-400"}>Hey whatsup!</div>
-  <div className="chat-footer   text-gray-600">Seen at 12:46</div>
-    </div>
-  )
+import {useAuthContext} from "../../context/AuthContext.jsx"
+import { extractTime } from "../../utils/extractTime.js";
+import useCoversation from "../../zustand/useConversation";
+
+const Message = ({message}) => {
+  const {authUser} = useAuthContext();
+  const {selectedConversation} = useCoversation();
+  const fromMe = message.senderId === authUser._id;
+  const formattedTime = extractTime(message.createdAt);
+  const chatClassName = fromMe ? 'chat-end' : 'chat-start';
+  const profilePic = fromMe ? authUser.profilepic:selectedConversation?.profilepic;
+  const bubbleBgColor = fromMe ? 'bg-blue-500': ''
+  const shakeClass = message.shouldShake ? "shake" : ""
+  return(
+		<div className={`chat ${chatClassName}`}>
+			<div className='chat-image avatar'>
+				<div className='w-10 rounded-full'>
+					<img alt='Tailwind CSS chat bubble component' src={profilePic} />
+				</div>
+			</div>
+			<div className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass}  pb-2`}>{message.message}</div>    
+			<div className=' text-black opacity-40 chat-footer text-xs flex gap-1 items-center'>{formattedTime}</div>
+		</div>
+	);
 }
 
 export default Message
